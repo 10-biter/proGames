2398c8e6bc9e63c11f889edb0b26b28a8e860139
10-biter <ep.student@hotmail.com>
2025-12-15 15:36:48 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 namespace proGames {
     // color=#400080 icon="\uf005"
 
-    export function setPlatformerModeOn(sprite:SpriteFlag): void {
-        
+    export function setPlatformerModeOn(sprite:Sprite): void {
+        sprite.ay = (500)
+        scene.cameraFollowSprite(sprite)
+        controller.moveSprite(sprite, 100, 0)
+
+
+        controller.A.onEvent(ControllerButtonEvent.Pressed, function() {
+            sprite.vy = (200)
+        })
     }
 }
